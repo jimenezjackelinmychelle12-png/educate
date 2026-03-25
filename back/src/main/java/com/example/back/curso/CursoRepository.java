2ad7e3195fbbc375c0d05e8e95bd0d673d80c617
jackej <jimenezjackelinmychelle12@gmail.com>
2026-03-25 15:38:40 -0400

    carpeta curso

---
+++ back/src/main/java/com/example/back/curso/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.example.back.curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<CursoEntity, Integer> {
+}
