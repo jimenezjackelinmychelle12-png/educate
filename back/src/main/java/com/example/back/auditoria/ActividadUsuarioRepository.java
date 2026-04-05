d90115cf3976deeac508031f719475ef8a5394de
jackej <jimenezjackelinmychelle12@gmail.com>
2026-04-05 00:52:32 -0400

    Add files via upload
    subir archivos java

---
+++ back/src/main/java/com/example/back/auditoria/ActividadUsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.example.back.auditoria;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActividadUsuarioRepository extends JpaRepository<ActividadUsuario, Integer> {
+}
