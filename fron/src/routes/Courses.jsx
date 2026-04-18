8ae9dd02646511aca784d9a6fbcf588d44c16759
Ro-Nel <rodrigo.saravia@ucb.edu.bo>
2026-04-17 20:29:37 -0400

    avances1 SCRUM-55

--- fron/src/routes/Courses.jsx
+++ fron/src/routes/Courses.jsx
@@ -48,7 +48,7 @@ function Courses() {
     */
   }, []);
 
-  
+
   if (loading) {
     return <h2 style={{ textAlign: "center" }}>Cargando cursos...</h2>;
   }
@@ -78,6 +78,7 @@ function Courses() {
     </div>
   );
 }
+
 
 const styles = {
   container: {
