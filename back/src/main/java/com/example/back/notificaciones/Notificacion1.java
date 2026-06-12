3a7c044673e98dba4ee03a7a6b3a3a5942e4ce74
jackej <jimenezjackelinmychelle12@gmail.com>
2026-06-12 17:30:58 -0400

    bandeja

---
+++ back/src/main/java/com/example/back/notificaciones/Notificacion1.java
@@ -0,0 +1,112 @@
+package com.example.back.notificaciones;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bandeja")
+public class Notificacion1 {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @Column(length = 1000)
+    private String mensaje;
+
+    private String emailEmisor;
+
+    private String emailDestino;
+
+    private LocalDateTime fechaEnvio;
+
+    private boolean leido;
+
+    private boolean archivado;
+
+    private boolean eliminado;
+
+    public Notificacion1() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getEmailEmisor() {
+        return emailEmisor;
+    }
+
+    public void setEmailEmisor(String emailEmisor) {
+        this.emailEmisor = emailEmisor;
+    }
+
+    public String getEmailDestino() {
+        return emailDestino;
+    }
+
+    public void setEmailDestino(String emailDestino) {
+        this.emailDestino = emailDestino;
+    }
+
+    public LocalDateTime getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(LocalDateTime fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public boolean isLeido() {
+        return leido;
+    }
+
+    public void setLeido(boolean leido) {
+        this.leido = leido;
+    }
+
+    public boolean isArchivado() {
+        return archivado;
+    }
+
+    public void setArchivado(boolean archivado) {
+        this.archivado = archivado;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+}
